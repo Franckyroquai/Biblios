4f825b641303fc9267fd750d67a96e87fa18083a
Franckyroquai <franckyroquai@gmail.com>
2024-07-12 19:13:27 +0200

    Ajout d'une checkbox de validation et de ses contraintes

--- src/Form/EditorType.php
+++ src/Form/EditorType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Editor;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class EditorType extends AbstractType
 {
@@ -14,6 +16,13 @@ class EditorType extends AbstractType
     {
         $builder
             ->add('name', TextType::class)
+            ->add('certification', CheckboxType::class, [
+                'mapped' => false,
+                'label' => "Je certifie l'exactitude des informations fournies",
+                'constraints' => [
+                    new Assert\IsTrue(message: "Vous devez cocher la case pour ajouter un livre."),
+                ],
+            ])
         ;
     }
 
